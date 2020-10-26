3707894361707a95403577796cca67b0468ba60a
krystek77 <krystian.wrona77@gmail.com>
2020-10-26 11:21:16 +0100

    Create styled component to handle theme

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components/macro';
+
+const theme = {
+  colors: {
+    dark: '#2e2e2e',
+    light: '#e2e2e2',
+    medium: '#f7f7f7',
+    primary_darker: '#8e0500',
+    primary: '#cc0800',
+    primary_lighter: '#d63933',
+    secondary_darker: '#2a364d',
+    secondary: '#3d4e6f',
+    secondary_lighter: '#63718b',
+  },
+};
+
+export { ThemeProvider, theme };
