25c5354db7347c467dd9c2a356a9d1f53b118075
krystek77 <krystian.wrona77@gmail.com>
2020-10-27 09:20:02 +0100

    Create list of question based on data set

--- src/containers/accordion.tsx
+++ src/containers/accordion.tsx
@@ -1,35 +1,22 @@
 import React from 'react';
 import { Accordion } from '../components';
+import dataFaqs from '../fixtures/faqs.json';
 
 export const AccordionContainer = () => {
   return (
     <Accordion.Container someProps={'10'}>
       <Accordion>
         <Accordion.Title>Frequenced Asked Questions</Accordion.Title>
-        <Accordion.List>
-          <Accordion.ListItem>
-            <Accordion.Header>Question one</Accordion.Header>
-            <Accordion.Body>
-              Praesent sapien massa, convallis a pellentesque nec, egestas non
-              nisi. Donec sollicitudin molestie malesuada. Vestibulum ante ipsum
-              primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-              Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit
-              amet ligula.
-            </Accordion.Body>
-          </Accordion.ListItem>
-        </Accordion.List>
-        <Accordion.List>
-          <Accordion.ListItem>
-            <Accordion.Header>Question two</Accordion.Header>
-            <Accordion.Body>
-              Praesent sapien massa, convallis a pellentesque nec, egestas non
-              nisi. Donec sollicitudin molestie malesuada. Vestibulum ante ipsum
-              primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-              Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit
-              amet ligula.
-            </Accordion.Body>
-          </Accordion.ListItem>
-        </Accordion.List>
+        {dataFaqs.map((item) => {
+          return (
+            <Accordion.List key={item.id}>
+              <Accordion.ListItem>
+                <Accordion.Header>{item.question}</Accordion.Header>
+                <Accordion.Body>{item.answer}</Accordion.Body>
+              </Accordion.ListItem>
+            </Accordion.List>
+          );
+        })}
       </Accordion>
     </Accordion.Container>
   );
