f0a24287adb3f2b9bdbc3309b334f9a190a5885c
krystek77 <krystian.wrona77@gmail.com>
2020-10-27 20:32:40 +0100

    Use constants to define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { HomePage, AdminPage, SigninPage, UserPage } from './pages';
+import { ADMIN, HOME, SIGNIN, USER } from './constants/routes';
 
 export function App() {
   return (
     <Router>
       {/** <div className="root"> all components</div> */}
-      <Route path='/signin'>
+      <Route path={SIGNIN}>
         <SigninPage />
       </Route>
-      <Route path='/user'>
+      <Route path={USER}>
         <UserPage />
       </Route>
-      <Route path='/admin'>
+      <Route path={ADMIN}>
         <AdminPage />
       </Route>
-      <Route exact path='/'>
+      <Route exact path={HOME}>
         <HomePage />
       </Route>
     </Router>
---
+++ src/constants/routes.ts
@@ -0,0 +1,4 @@
+export const HOME = '/';
+export const SIGNIN = '/signin';
+export const ADMIN = '/admin';
+export const USER = '/user';
