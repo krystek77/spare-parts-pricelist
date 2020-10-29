db602dbc29cd8c0d9d4add7983b7d29213f1a291
krystek77 <krystian.wrona77@gmail.com>
2020-10-29 12:30:28 +0100

    Set up firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+console.log('SET UP FIREBASE');
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN_FIREBASE,
+  databaseURL: process.env.REACT_APP_DATABASE_URL_FIREBASE,
+  projectId: process.env.REACT_APP_PROJECT_ID_FIREBASE,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_FIREBASE,
+  appId: process.env.REACT_APP_APP_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const dataBase = firebase.firestore();
+const auth = firebase.auth();
+
+export { firebase, dataBase, auth };
