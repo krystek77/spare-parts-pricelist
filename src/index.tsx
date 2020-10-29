e313f4b6f249236ac0275d651a1252d9c00c3be6
krystek77 <krystian.wrona77@gmail.com>
2020-10-29 12:31:06 +0100

    Update main index.tsx
    Firebase setting checked and uncomment then

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import { firebase } from './lib/firebase';
 import { render } from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './App';
 import { ThemeProvider, theme } from './theme';
+
+// function fetchData() {
+//   firebase
+//     .firestore()
+//     .collection('spare-parts')
+//     .add({ id: 1, name: 'Uszczelka' });
+// }
+
+// fetchData();
 
 render(
   <React.Fragment>
