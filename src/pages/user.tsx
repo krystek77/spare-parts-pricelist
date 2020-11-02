c00fec9d422e1197380c4dd2a8067e649a2cda48
krystek77 <krystian.wrona77@gmail.com>
2020-11-02 16:17:24 +0100

    Finish layout of user page

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import { NavigationContainer } from '../containers';
+import {
+  MainContainer,
+  NavigationContainer,
+  SidebarContainer,
+} from '../containers';
+import { Navigation } from '../components';
+import * as ROUTES from '../constants/routes';
+
 interface IUserPage {}
 export const UserPage: React.FC<IUserPage> = () => {
-  return <NavigationContainer isAuthenticated bgColor />;
+  return (
+    <React.Fragment>
+      <NavigationContainer bgColor>
+        <Navigation.ButtonLink to={ROUTES.HOME}>sign out</Navigation.ButtonLink>
+      </NavigationContainer>
+      <SidebarContainer>SIDEBAR USER ....</SidebarContainer>
+      <MainContainer>MAIN CONTAINER</MainContainer>
+    </React.Fragment>
+  );
 };
