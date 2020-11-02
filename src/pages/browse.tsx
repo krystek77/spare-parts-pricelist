b04267ebac02598e613bac2398ac105a40b5575f
krystek77 <krystian.wrona77@gmail.com>
2020-11-02 16:20:16 +0100

    Finish template of browse page

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { NavigationContainer } from '../containers';
+import {
+  MainContainer,
+  NavigationContainer,
+  SidebarContainer,
+} from '../containers';
 import { Navigation } from '../components';
 import * as ROUTES from '../constants/routes';
-interface IBrowsePage {}
+
+interface IBrowsePage {
+  role?: string;
+}
 export const BrowsePage: React.FC<IBrowsePage> = ({
   children,
+  role = 'admin',
   ...restProps
 }) => {
   // console.log('Browse Page - restProps: ', restProps);
   return (
-    <NavigationContainer bgColor>
-      <Navigation.ButtonLink to={ROUTES.HOME}>sign out</Navigation.ButtonLink>
-    </NavigationContainer>
+    <React.Fragment>
+      <NavigationContainer bgColor>
+        {role === 'admin' && (
+          <Navigation.ButtonLink to={ROUTES.ADMIN}>ADMIN</Navigation.ButtonLink>
+        )}
+        {role === 'user' && (
+          <Navigation.ButtonLink to={ROUTES.USER}>USER</Navigation.ButtonLink>
+        )}
+        <Navigation.ButtonLink to={ROUTES.HOME}>sign out</Navigation.ButtonLink>
+      </NavigationContainer>
+      <SidebarContainer>BROWSE FOR ALL ...</SidebarContainer>
+      <MainContainer>MAIN CONTAINER</MainContainer>
+    </React.Fragment>
   );
 };
