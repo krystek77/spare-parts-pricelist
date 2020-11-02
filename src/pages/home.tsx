3c5ed67c9da7d7a1643ce0486f8bd2a8b60118ac
krystek77 <krystian.wrona77@gmail.com>
2020-11-02 16:20:48 +0100

    Add navigation conatiner to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,14 +4,40 @@ import {
   JumbotronContainer,
   TabsContainer,
   HeaderContainer,
+  NavigationContainer,
 } from '../containers';
+import { Header, Newsletter, Navigation } from '../components';
+import * as ROUTES from '../constants/routes';
 
 interface IHomePage {}
 
 export const HomePage: React.FC<IHomePage> = () => {
   return (
     <React.Fragment>
-      <HeaderContainer bgImage />
+      <HeaderContainer bgImage>
+        <NavigationContainer>
+          <Navigation.ButtonLink to={ROUTES.SIGNIN}>
+            sign in
+          </Navigation.ButtonLink>
+        </NavigationContainer>
+        <Header.Content>
+          <Header.Title>
+            Browse the spare parts priclist anytime and anywhere.
+          </Header.Title>
+          <Header.SubTitle>
+            Add, delete, edit pricelist category and spare parts
+          </Header.SubTitle>
+          <Newsletter>
+            <Newsletter.Input type='text' placeholder='Enter your email' />
+            <Newsletter.Button type='button'>Subscribe</Newsletter.Button>
+            <Newsletter.Break />
+            <Newsletter.Text>
+              We are starting to be up to date. Enter email and subscribe to
+              newsletter
+            </Newsletter.Text>
+          </Newsletter>
+        </Header.Content>
+      </HeaderContainer>
       <TabsContainer />
       <JumbotronContainer />
       <AccordionContainer />
