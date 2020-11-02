678f6dcfa982a81dd63b63b1de7b390ef630bffb
krystek77 <krystian.wrona77@gmail.com>
2020-11-02 16:14:07 +0100

    Add imports of main and sidebar components

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -3,10 +3,14 @@ import { TabsContainer } from './tabs';
 import { AccordionContainer } from './accordion';
 import { NavigationContainer } from './navigation';
 import { HeaderContainer } from './header';
+import { SidebarContainer } from './sidebar';
+import { MainContainer } from './main';
 export {
   NavigationContainer,
   JumbotronContainer,
   TabsContainer,
   AccordionContainer,
   HeaderContainer,
+  SidebarContainer,
+  MainContainer,
 };
