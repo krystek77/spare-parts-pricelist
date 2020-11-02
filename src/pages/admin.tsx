d29931dbc8ad23a6e35c507431e52d0ec954aa72
krystek77 <krystian.wrona77@gmail.com>
2020-11-02 16:18:04 +0100

    Finish template of admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
-import { NavigationContainer } from '../containers';
+import {
+  MainContainer,
+  NavigationContainer,
+  SidebarContainer,
+} from '../containers';
+import { Navigation, Sidebar } from '../components';
+import * as ROUTES from '../constants/routes';
 
 interface IAdminPage {}
 export const AdminPage: React.FC<IAdminPage> = () => {
-  return <NavigationContainer isAuthenticated bgColor />;
+  return (
+    <React.Fragment>
+      <NavigationContainer bgColor>
+        <Navigation.ButtonLink to={ROUTES.HOME}>sign out</Navigation.ButtonLink>
+      </NavigationContainer>
+      <SidebarContainer>
+        SIDEBAR ADMIN ...
+        <Sidebar.ButtonLink to={ROUTES.ADD_USER}>Add User</Sidebar.ButtonLink>
+        <Sidebar.ButtonLink to={ROUTES.BROWSE}>
+          Browse PriceLists
+        </Sidebar.ButtonLink>
+      </SidebarContainer>
+      <MainContainer>MAIN CONTAINER</MainContainer>
+    </React.Fragment>
+  );
 };
