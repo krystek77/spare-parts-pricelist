a2504feb883a30a3aa463d1487a458fd2a83c2e3
krystek77 <krystian.wrona77@gmail.com>
2020-11-03 11:06:51 +0100

    Add form to add another one user to add user page

--- src/pages/addUser.tsx
+++ src/pages/addUser.tsx
@@ -1,14 +1,104 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { auth, dataBase } from '../lib/firebase';
 import {
   MainContainer,
   NavigationContainer,
   SidebarContainer,
 } from '../containers';
-import { Navigation, Sidebar } from '../components';
+import { Navigation, Sidebar, Form } from '../components';
 import * as ROUTES from '../constants/routes';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const isEmpty = (str: string): boolean => {
+  if (str.trim() === '') return true;
+  return false;
+};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const isEmail = (email: string): boolean => {
+  const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return email.match(regExp) !== null ? true : false;
+};
+const checkLength = (str: string, min: number, max: number): boolean => {
+  return min <= str.length && str.length <= max;
+};
+
+function emailValidation(email: string): boolean {
+  let isValid: boolean = true;
+  isValid = !isEmpty(email) && isValid;
+  isValid = isEmail(email) && isValid;
+  return isValid;
+}
+
+function passwordValidation(password: string) {
+  let isValid: boolean = true;
+  isValid = !isEmpty(password) && isValid;
+  isValid = checkLength(password, 6, 10) && isValid;
+  return isValid;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function dataValidation(email: string, password: string) {
+  let isValid: boolean = emailValidation(email) && passwordValidation(password);
+  return isValid;
+}
+
 interface IAddUser {}
+interface IErrors {
+  email?: string;
+  password?: string;
+  confirmedPassword?: string;
+  server?: string;
+}
+enum ROLES {
+  ADMIN = 'admin',
+  USER = 'user',
+  TESTER = 'tester',
+}
+
 export const AddUser: React.FC<IAddUser> = () => {
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+  const [confiremdPassword, setConfiremdPassword] = React.useState<string>('');
+  const [nick, setNick] = React.useState<string>('');
+  const [errors, setErrors] = React.useState<IErrors>({});
+  const [userRole, setUserRole] = React.useState<string>(ROLES.USER);
+  const history = useHistory();
+
+  const validForm =
+    dataValidation(email, password) &&
+    password === confiremdPassword &&
+    checkLength(nick, 4, 8);
+  const isError = Object.keys(errors).length > 0 ? true : false;
+
+  const handleAddUser = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((data) => {
+        const { user } = data;
+        if (user) {
+          const newUser = {
+            userID: user.uid,
+            email: user.email,
+            avatar: 'default_avatar',
+            displayName: nick,
+            role: userRole,
+          };
+          user.updateProfile({
+            displayName: nick,
+            photoURL: 'default_avatar',
+          });
+          return dataBase.collection('users').add(newUser);
+        }
+      })
+      .then((user) => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => setErrors({ ...errors, server: error.message }));
+  };
+
   return (
     <React.Fragment>
       <NavigationContainer bgColor>
@@ -21,7 +111,105 @@ export const AddUser: React.FC<IAddUser> = () => {
           Browse PriceLists
         </Sidebar.ButtonLink>
       </SidebarContainer>
-      <MainContainer>MAIN CONTAINER - ADD USER</MainContainer>
+      <MainContainer>
+        {isError && errors.server && (
+          <Form.ErrorServer>{errors.server}</Form.ErrorServer>
+        )}
+        <Form>
+          <Form.Title>Add User</Form.Title>
+          <Form.BaseForm onSubmit={handleAddUser}>
+            <Form.Input
+              type='text'
+              name='email'
+              placeholder='Email address'
+              value={email}
+              onChange={(e) => {
+                setErrors({});
+                setEmail(e.currentTarget.value);
+              }}
+            />
+            <Form.Input
+              type='text'
+              name='password'
+              placeholder='Enter password'
+              value={password}
+              onChange={(e) => {
+                setErrors({});
+                setPassword(e.currentTarget.value);
+              }}
+            />
+            <Form.Input
+              type='text'
+              name='confiremdPassword'
+              value={confiremdPassword}
+              placeholder='Confirm the password'
+              onChange={(e) => {
+                setErrors({});
+                setConfiremdPassword(e.currentTarget.value);
+              }}
+            />
+            <Form.Input
+              type='text'
+              name='name'
+              placeholder='User nick'
+              value={nick}
+              onChange={(e) => {
+                setErrors({});
+                setNick(e.currentTarget.value);
+              }}
+            />
+            <Form.Break />
+            <Form.SubmitButton type='submit' disabled={!validForm}>
+              Add User
+            </Form.SubmitButton>
+            <Form.Break />
+            <Form.InputsGroup>
+              <Form.RadioInput
+                type='radio'
+                id={ROLES.ADMIN}
+                value={ROLES.ADMIN}
+                name={ROLES.ADMIN}
+                checked={userRole === ROLES.ADMIN}
+                onChange={(e) => setUserRole(e.currentTarget.value)}
+              />
+              <Form.InputLabel htmlFor={ROLES.ADMIN}>
+                {ROLES.ADMIN}
+              </Form.InputLabel>
+            </Form.InputsGroup>
+            <Form.InputsGroup>
+              <Form.RadioInput
+                type='radio'
+                id={ROLES.USER}
+                value={ROLES.USER}
+                name={ROLES.USER}
+                checked={userRole === ROLES.USER}
+                onChange={(e) => setUserRole(e.currentTarget.value)}
+              />
+              <Form.InputLabel htmlFor={ROLES.USER}>
+                {ROLES.USER}
+              </Form.InputLabel>
+            </Form.InputsGroup>
+            <Form.InputsGroup>
+              <Form.RadioInput
+                type='radio'
+                id={ROLES.TESTER}
+                value={ROLES.TESTER}
+                name={ROLES.TESTER}
+                checked={userRole === ROLES.TESTER}
+                onChange={(e) => setUserRole(e.currentTarget.value)}
+              />
+              <Form.InputLabel htmlFor={ROLES.TESTER}>
+                {ROLES.TESTER}
+              </Form.InputLabel>
+            </Form.InputsGroup>
+            <Form.Break />
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you are not a
+              bot.
+            </Form.TextSmall>
+          </Form.BaseForm>
+        </Form>
+      </MainContainer>
     </React.Fragment>
   );
 };
