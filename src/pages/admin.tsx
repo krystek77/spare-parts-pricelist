21dc295344af906ba75aa18c2d8fbf87fbdbbea8
krystek77 <krystian.wrona77@gmail.com>
2020-11-05 10:12:20 +0100

    Add displaying spare parts for admin user

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -7,14 +7,19 @@ import {
 } from '../containers';
 import { Navigation, Sidebar } from '../components';
 import * as ROUTES from '../constants/routes';
-import { useAuth, usePriceLists } from '../hooks';
+import { useAuth, usePriceLists, useSpareParts } from '../hooks';
 import { useSelectedPriceListsContextValue } from '../context';
 
 interface IAdminPage {}
 export const AdminPage: React.FC<IAdminPage> = () => {
   const { authUser, setAuthUser, initialValue } = useAuth();
   const { priceLists } = usePriceLists(authUser.userID); //Maybe from localStorage
-  const { setSelectedPriceLists } = useSelectedPriceListsContextValue();
+  const {
+    selectedPriceLists,
+    setSelectedPriceLists,
+  } = useSelectedPriceListsContextValue();
+  const { spareParts } = useSpareParts(selectedPriceLists, authUser.userID);
+
   return (
     <React.Fragment>
       <NavigationContainer bgColor>
@@ -60,7 +65,51 @@ export const AdminPage: React.FC<IAdminPage> = () => {
           </ul>
         )}
       </SidebarContainer>
-      <MainContainer>MAIN CONTAINER</MainContainer>
+      <MainContainer>
+        {spareParts.length > 0 && (
+          <ul>
+            {spareParts.map((item) => {
+              return (
+                <li key={item.sparePartId}>
+                  <h2>{item.model}</h2>
+                  <h3>
+                    <div>
+                      <span>Spare part name</span>
+                    </div>
+                    <div>
+                      <span>From year</span>From year
+                    </div>
+                    <div>
+                      <span>To year</span>
+                    </div>
+                    <div>
+                      <span>Purchase price</span>
+                      <span>{`[${item.currency}]`}</span>
+                    </div>
+                    <div>
+                      <span>Is calculated</span>
+                    </div>
+                    <div>
+                      <span>Selling price</span>
+                      <span>[ZŁ]</span>
+                    </div>
+                  </h3>
+                  <div>
+                    <span>{item.name}</span>
+                    <span>{item.from}</span>
+                    <span>{item.to}</span>
+                    <span>{item.purchasePrice}</span>
+                    <span>{item.isCalculated ? 'YES' : 'NO'}</span>
+                    <span>{item.sellingPrice}</span>
+                  </div>
+                  <p>{item.description}</p>
+                  <p>{item.comments}</p>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </MainContainer>
     </React.Fragment>
   );
 };
