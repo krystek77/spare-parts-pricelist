d5e272984a71e8aba3b4cf217d979756cc6a158a
krystek77 <krystian.wrona77@gmail.com>
2020-11-06 14:07:21 +0100

    Update addSparePatr.tsx
    The form to add has been created

--- src/pages/addSparePart.tsx
+++ src/pages/addSparePart.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaClipboardList } from 'react-icons/fa';
 import { useAuth } from '../hooks';
 import { auth } from '../lib/firebase';
 import {
@@ -6,13 +7,30 @@ import {
   NavigationContainer,
   SidebarContainer,
 } from '../containers';
-import { Navigation, ListItems } from '../components';
+import { Navigation, ListItems, Form } from '../components';
 import * as ROUTES from '../constants/routes';
+import { CURRENCY } from '../helpers';
 
 interface IAddSparePart {}
 
 export const AddSparePart: React.FC<IAddSparePart> = () => {
+  const [name, setName] = React.useState<string>('');
+  const [model, setModel] = React.useState<string>('');
+  const [from, setFrom] = React.useState<string>('');
+  const [to, setTo] = React.useState<string>('');
+  const [purchasePrice, setPurchasePrice] = React.useState<string>('');
+  const [currency, setCurrency] = React.useState<string>(CURRENCY.PL);
+  const [priceListName, setPriceListName] = React.useState<string>('');
+  const [showPriceListsOverlay, setShowPriceListsOverlay] = React.useState<
+    boolean
+  >(false);
+
   const { setAuthUser, initialValue } = useAuth();
+
+  const handleAddSpareParts = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log('ADD SPARE PARTS');
+  };
 
   return (
     <React.Fragment>
@@ -64,7 +82,143 @@ export const AddSparePart: React.FC<IAddSparePart> = () => {
         </ListItems>
         {/** LINKS */}
       </SidebarContainer>
-      <MainContainer>ADD SPARE PART</MainContainer>
+      <MainContainer>
+        <Form>
+          <Form.Title>Add Spare Parts</Form.Title>
+          <Form.BaseForm onSubmit={handleAddSpareParts}>
+            <Form.InputsGroup>
+              <Form.InputLabel htmlFor='name'>Name:</Form.InputLabel>
+              <Form.Input
+                type='text'
+                name='name'
+                id='name'
+                value={name}
+                placeholder='Enter spare part name'
+                onChange={(e) => setName(e.currentTarget.value)}
+                onKeyDown={(e) => setName(e.currentTarget.value)}
+              />
+            </Form.InputsGroup>
+            <Form.InputsGroup>
+              <Form.InputLabel htmlFor='From'>Model</Form.InputLabel>
+              <Form.Input
+                type='text'
+                name='model'
+                id='model'
+                value={model}
+                placeholder='Enter machine model'
+                onChange={(e) => setModel(e.currentTarget.value)}
+                onKeyDown={(e) => setModel(e.currentTarget.value)}
+              />
+            </Form.InputsGroup>
+            <Form.InputsGroup>
+              <Form.InputsGroup>
+                <Form.InputLabel htmlFor='from'>From:</Form.InputLabel>
+                <Form.Input
+                  type='text'
+                  name='from'
+                  id='from'
+                  value={from}
+                  placeholder='1998'
+                  onChange={(e) => setFrom(e.currentTarget.value)}
+                  onKeyDown={(e) => setFrom(e.currentTarget.value)}
+                />
+              </Form.InputsGroup>
+              <Form.InputsGroup>
+                <Form.InputLabel htmlFor='to'>to:</Form.InputLabel>
+                <Form.Input
+                  type='text'
+                  name='to'
+                  id='to'
+                  value={to}
+                  placeholder='2010'
+                  onChange={(e) => setTo(e.currentTarget.value)}
+                  onKeyDown={(e) => setTo(e.currentTarget.value)}
+                />
+              </Form.InputsGroup>
+            </Form.InputsGroup>
+            <Form.Break />
+            <Form.InputsGroup>
+              <Form.InputsGroup>
+                <Form.InputLabel htmlFor='pricList'>
+                  PricList Category:
+                </Form.InputLabel>
+                <Form.Input
+                  type='text'
+                  name='pricList'
+                  id='pricList'
+                  value={priceListName}
+                  placeholder='Enter price list name ...'
+                  onChange={(e) => setPriceListName(e.currentTarget.value)}
+                  onKeyDown={(e) => setPriceListName(e.currentTarget.value)}
+                />
+              </Form.InputsGroup>
+              <Form.InputsGroup>
+                <Form.IconButton
+                  type='button'
+                  onClick={() =>
+                    setShowPriceListsOverlay(!showPriceListsOverlay)
+                  }
+                  onKeyDown={() =>
+                    setShowPriceListsOverlay(!showPriceListsOverlay)
+                  }
+                >
+                  <FaClipboardList />
+                </Form.IconButton>
+              </Form.InputsGroup>
+            </Form.InputsGroup>
+            <Form.Break />
+            <Form.InputsGroup>
+              <Form.InputLabel htmlFor='purchase'>
+                Purchase price:
+              </Form.InputLabel>
+              <Form.Input
+                type='number'
+                name='purchase'
+                id='purchase'
+                value={purchasePrice}
+                placeholder='100.10'
+                onChange={(e) => setPurchasePrice(e.currentTarget.value)}
+                onKeyDown={(e) => setPurchasePrice(e.currentTarget.value)}
+              />
+            </Form.InputsGroup>
+
+            <Form.Break />
+            <Form.InputsGroup>
+              <Form.RadioInput
+                type='radio'
+                id={CURRENCY.PL}
+                value={CURRENCY.PL}
+                name={CURRENCY.PL}
+                checked={currency === CURRENCY.PL}
+                onChange={(e) => setCurrency(e.currentTarget.value)}
+                onKeyDown={(e) => setCurrency(e.currentTarget.value)}
+              />
+              <Form.InputLabel htmlFor={CURRENCY.PL}>
+                Purchase price given in <strong>{CURRENCY.PL}</strong>
+              </Form.InputLabel>
+            </Form.InputsGroup>
+            <Form.InputsGroup>
+              <Form.RadioInput
+                type='radio'
+                id={CURRENCY.EUR}
+                value={CURRENCY.EUR}
+                name={CURRENCY.EUR}
+                checked={currency === CURRENCY.EUR}
+                onChange={(e) => setCurrency(e.currentTarget.value)}
+                onKeyDown={(e) => setCurrency(e.currentTarget.value)}
+              />
+              <Form.InputLabel htmlFor='EUR'>
+                Purchase price given in <strong>{CURRENCY.EUR}</strong>
+              </Form.InputLabel>
+            </Form.InputsGroup>
+            <Form.Break />
+            <Form.SubmitButton type='submit' disabled={false}>
+              Add Spare Parts
+            </Form.SubmitButton>
+            <Form.Break />
+          </Form.BaseForm>
+        </Form>
+      </MainContainer>
     </React.Fragment>
   );
 };
