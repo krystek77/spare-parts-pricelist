aad4409af48402845aca8352c56632da9dc21c8c
krystek77 <krystian.wrona77@gmail.com>
2020-11-06 09:59:19 +0100

    Add exchange rate context to parent module

--- src/context/index.ts
+++ src/context/index.ts
@@ -4,9 +4,19 @@ import {
   SelectedPriceListsContextConsumer,
   useSelectedPriceListsContextValue,
 } from './selected-pricelists-context';
+import {
+  ExchangeRateContext,
+  ExchangeRateContextConsumer,
+  ExchangeRateContextProvider,
+  useExchangeRateContext,
+} from './exchange-rate';
 export {
   SelectedPriceListsContext,
   SelectedPriceListsContextProvider,
   SelectedPriceListsContextConsumer,
   useSelectedPriceListsContextValue,
+  ExchangeRateContext,
+  ExchangeRateContextConsumer,
+  ExchangeRateContextProvider,
+  useExchangeRateContext,
 };
