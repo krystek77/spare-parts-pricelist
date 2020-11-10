dc9fdfc868b467e9e1d9fc6732ffbc44977c9a99
krystek77 <krystian.wrona77@gmail.com>
2020-11-10 18:19:29 +0100

    Prepare basic layout of edit spare part page

--- src/pages/editSparePart.tsx
+++ src/pages/editSparePart.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { auth } from '../lib/firebase';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import {
+  NavigationContainer,
+  SidebarContainer,
+  MainContainer,
+  InfoContainer,
+} from '../containers';
+import { Navigation, ListItems, ContentTitle } from '../components';
+import { useAuth } from '../hooks';
+import { useSelectedPriceListsContextValue } from '../context';
+import * as ROUTES from '../constants/routes';
 interface IRoutesProps {
   /**
    * routes props
@@ -13,7 +24,75 @@ interface IEditSparePartPage extends RouteComponentProps<IRoutesProps> {
 }
 
 const EditPage: React.FC<IEditSparePartPage> = (props) => {
-  return <div>EDIT SPARE PART PAGE</div>;
+  const { setAuthUser, initialValue } = useAuth();
+  const { setSelectedPriceLists } = useSelectedPriceListsContextValue();
+  const { match } = props;
+  return (
+    <React.Fragment>
+      <NavigationContainer bgColor>
+        <Navigation.SignoutButton
+          type='button'
+          onClick={() => {
+            auth
+              .signOut()
+              .then(() => {
+                localStorage.removeItem('authUser');
+                setAuthUser(initialValue);
+              })
+              .catch((error) => {
+                console.log('Sign out failed');
+              });
+          }}
+        >
+          sign out
+        </Navigation.SignoutButton>
+      </NavigationContainer>
+      <SidebarContainer>
+        {/** LINKS */}
+        <ListItems>
+          <ListItems.Title>LINKS</ListItems.Title>
+          <ListItems.List>
+            <ListItems.ListItem>
+              <ListItems.ListItemButtonLink to={ROUTES.ADD_USER}>
+                Add User
+              </ListItems.ListItemButtonLink>
+            </ListItems.ListItem>
+            <ListItems.ListItem>
+              <ListItems.ListItemButtonLink to={ROUTES.ADD_SPARE_PART}>
+                Add Spare Part
+              </ListItems.ListItemButtonLink>
+            </ListItems.ListItem>
+            <ListItems.ListItem>
+              <ListItems.ListItemButtonLink
+                to={ROUTES.BROWSE}
+                onClick={() => setSelectedPriceLists('')}
+              >
+                Browese PriceLists
+              </ListItems.ListItemButtonLink>
+            </ListItems.ListItem>
+            <ListItems.ListItem>
+              <ListItems.ListItemButtonLink to={ROUTES.ADMIN}>
+                Admin
+              </ListItems.ListItemButtonLink>
+            </ListItems.ListItem>
+          </ListItems.List>
+        </ListItems>
+        {/** LINKS */}
+        <InfoContainer />
+        {/** INFO */}
+      </SidebarContainer>
+      <MainContainer>
+        {/** CONTENT TITLE */}
+        <ContentTitle>
+          <ContentTitle.BaseTitle>EDIT SPARE PART</ContentTitle.BaseTitle>
+          <ContentTitle.SubTitle>
+            ...{match.params.sparePartID} ...
+          </ContentTitle.SubTitle>
+        </ContentTitle>
+        {/**  CONTENT TITLE */}
+      </MainContainer>
+    </React.Fragment>
+  );
 };
 
 export const EditSparePartPage = withRouter(EditPage);
