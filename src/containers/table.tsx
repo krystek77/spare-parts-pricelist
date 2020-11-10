56c573707a773801a19ab7f9fe92faf93eae1dc8
krystek77 <krystian.wrona77@gmail.com>
2020-11-10 12:51:01 +0100

    Display info about loading data

--- src/containers/table.tsx
+++ src/containers/table.tsx
@@ -12,109 +12,107 @@ interface ITableContainer {
 export const TableContainer: React.FC<ITableContainer> = ({ ...restProps }) => {
   const { list, handleDelete, message } = restProps;
 
-  return (
-    <Table>
-      {message && <Table.Message>{message}</Table.Message>}
-      <Table.BaseTable>
-        {list && list.length > 0 && (
-          <>
-            <Table.HeaderRowTable>
-              <Table.OrdinaryNumberColTable>Lp.</Table.OrdinaryNumberColTable>
-              <Table.HeaderContentRowTable>
-                <Table.HeaderColTable>Name</Table.HeaderColTable>
-                <Table.HeaderColTable>Model</Table.HeaderColTable>
-                <Table.HeaderColTable>From</Table.HeaderColTable>
-                <Table.HeaderColTable>To</Table.HeaderColTable>
-                <Table.HeaderColTable>Purchase netto</Table.HeaderColTable>
-                <Table.HeaderColTable>Selling netto</Table.HeaderColTable>
-                <Table.HeaderColTable>GROSS</Table.HeaderColTable>
-              </Table.HeaderContentRowTable>
-            </Table.HeaderRowTable>
-            {list.map((item, index) => {
-              return (
-                <Table.BodyTable key={item.sparePartID}>
-                  <Table.RowTable>
-                    <Table.OrdinaryNumberColTable>
-                      {index + 1}
-                    </Table.OrdinaryNumberColTable>
-                    <Table.ContentColTable>
-                      <Table.HeaderContentColTable>
-                        <Table.HeaderContentFieldColTable>
-                          {item.name}
-                        </Table.HeaderContentFieldColTable>
-                        <Table.HeaderContentFieldColTable>
-                          {item.model}
-                        </Table.HeaderContentFieldColTable>
-                        <Table.HeaderContentFieldColTable>
-                          {item.from}
-                        </Table.HeaderContentFieldColTable>
-                        <Table.HeaderContentFieldColTable>
-                          {item.to}
-                        </Table.HeaderContentFieldColTable>
-                        <Table.HeaderContentFieldColTable>
-                          {item.purchasePrice === 0
-                            ? '-'
-                            : `${item.purchasePrice.toFixed(2)} ${
-                                item.currency
-                              }`}
-                        </Table.HeaderContentFieldColTable>
-                        <Table.HeaderContentFieldColTable>
-                          {`${item.sellingPrice.toFixed(2)} zł`}
-                        </Table.HeaderContentFieldColTable>
-                        <Table.HeaderContentFieldColTable>
-                          {`${(item.sellingPrice * 1.23).toFixed(2)} zł`}
-                        </Table.HeaderContentFieldColTable>
-                      </Table.HeaderContentColTable>
-                      {item.description && (
-                        <Table.DescriptionContentColTable>
-                          {`DESC: ${item.description}`}
-                        </Table.DescriptionContentColTable>
-                      )}
-                      {item.comments && (
-                        <Table.CommentsContentsColTable>
-                          {`COMMENT: ${item.comments}`}
-                        </Table.CommentsContentsColTable>
-                      )}
-                    </Table.ContentColTable>
-                  </Table.RowTable>
-                  <Table.Controls>
-                    <Table.ControlButton
-                      btn='DESCRIPTION'
-                      type='button'
-                      title='Show description'
-                    >
-                      <MdDescription />
-                    </Table.ControlButton>
-                    <Table.ControlButton
-                      btn='COMMENT'
-                      type='button'
-                      title='Show comment'
-                    >
-                      <FaCommentAlt />
-                    </Table.ControlButton>
-                    <Table.ControlButtonDelete
-                      type='button'
-                      title='Delete'
-                      onClick={() =>
-                        handleDelete && handleDelete(item.sparePartID)
-                      }
-                    >
-                      <FaTrashAlt />
-                    </Table.ControlButtonDelete>
-                    <Table.ControlButtonEdit
-                      type='button'
-                      title='Edit'
-                      onClick={() => console.log('EDIT')}
-                    >
-                      <FaEdit />
-                    </Table.ControlButtonEdit>
-                  </Table.Controls>
-                </Table.BodyTable>
-              );
-            })}
-          </>
-        )}
-      </Table.BaseTable>
-    </Table>
-  );
+  const content =
+    list && list.length > 0 ? (
+      <Table>
+        {message && <Table.Message>{message}</Table.Message>}
+        <Table.BaseTable>
+          <Table.HeaderRowTable>
+            <Table.OrdinaryNumberColTable>Lp.</Table.OrdinaryNumberColTable>
+            <Table.HeaderContentRowTable>
+              <Table.HeaderColTable>Name</Table.HeaderColTable>
+              <Table.HeaderColTable>Model</Table.HeaderColTable>
+              <Table.HeaderColTable>From</Table.HeaderColTable>
+              <Table.HeaderColTable>To</Table.HeaderColTable>
+              <Table.HeaderColTable>Purchase netto</Table.HeaderColTable>
+              <Table.HeaderColTable>Selling netto</Table.HeaderColTable>
+              <Table.HeaderColTable>GROSS</Table.HeaderColTable>
+            </Table.HeaderContentRowTable>
+          </Table.HeaderRowTable>
+          {list.map((item, index) => {
+            return (
+              <Table.BodyTable key={item.sparePartID}>
+                <Table.RowTable>
+                  <Table.OrdinaryNumberColTable>
+                    {index + 1}
+                  </Table.OrdinaryNumberColTable>
+                  <Table.ContentColTable>
+                    <Table.HeaderContentColTable>
+                      <Table.HeaderContentFieldColTable>
+                        {item.name}
+                      </Table.HeaderContentFieldColTable>
+                      <Table.HeaderContentFieldColTable>
+                        {item.model}
+                      </Table.HeaderContentFieldColTable>
+                      <Table.HeaderContentFieldColTable>
+                        {item.from}
+                      </Table.HeaderContentFieldColTable>
+                      <Table.HeaderContentFieldColTable>
+                        {item.to}
+                      </Table.HeaderContentFieldColTable>
+                      <Table.HeaderContentFieldColTable>
+                        {item.purchasePrice === 0
+                          ? '-'
+                          : `${item.purchasePrice.toFixed(2)} ${item.currency}`}
+                      </Table.HeaderContentFieldColTable>
+                      <Table.HeaderContentFieldColTable>
+                        {`${item.sellingPrice.toFixed(2)} zł`}
+                      </Table.HeaderContentFieldColTable>
+                      <Table.HeaderContentFieldColTable>
+                        {`${(item.sellingPrice * 1.23).toFixed(2)} zł`}
+                      </Table.HeaderContentFieldColTable>
+                    </Table.HeaderContentColTable>
+                    {item.description && (
+                      <Table.DescriptionContentColTable>
+                        {`DESC: ${item.description}`}
+                      </Table.DescriptionContentColTable>
+                    )}
+                    {item.comments && (
+                      <Table.CommentsContentsColTable>
+                        {`COMMENT: ${item.comments}`}
+                      </Table.CommentsContentsColTable>
+                    )}
+                  </Table.ContentColTable>
+                </Table.RowTable>
+                <Table.Controls>
+                  <Table.ControlButton
+                    btn='DESCRIPTION'
+                    type='button'
+                    title='Show description'
+                  >
+                    <MdDescription />
+                  </Table.ControlButton>
+                  <Table.ControlButton
+                    btn='COMMENT'
+                    type='button'
+                    title='Show comment'
+                  >
+                    <FaCommentAlt />
+                  </Table.ControlButton>
+                  <Table.ControlButtonDelete
+                    type='button'
+                    title='Delete'
+                    onClick={() =>
+                      handleDelete && handleDelete(item.sparePartID)
+                    }
+                  >
+                    <FaTrashAlt />
+                  </Table.ControlButtonDelete>
+                  <Table.ControlButtonEdit
+                    type='button'
+                    title='Edit'
+                    onClick={() => console.log('EDIT')}
+                  >
+                    <FaEdit />
+                  </Table.ControlButtonEdit>
+                </Table.Controls>
+              </Table.BodyTable>
+            );
+          })}
+        </Table.BaseTable>
+      </Table>
+    ) : (
+      <div>Lodaing data...</div>
+    );
+  return content;
 };
