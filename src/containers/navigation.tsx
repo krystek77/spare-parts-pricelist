5868491703bca432c1b7b8f27b7d09270d369447
krystek77 <krystian.wrona77@gmail.com>
2020-11-12 10:16:50 +0100

    Display the user nick on the navigation bar

--- src/containers/navigation.tsx
+++ src/containers/navigation.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import * as ROUTES from '../constants/routes';
 import { Navigation } from '../components';
 import logo from '../logo.svg';
+import { getAuthUser } from '../helpers';
 interface INavigationContainer {
   bgColor?: boolean;
 }
+
 export const NavigationContainer: React.FC<INavigationContainer> = ({
   children,
   bgColor = false,
   ...restProps
 }) => {
+  const authUser = getAuthUser();
   return (
     <>
       <Navigation.Container bgColor={bgColor}>
@@ -20,6 +23,9 @@ export const NavigationContainer: React.FC<INavigationContainer> = ({
               <Navigation.LogoText>Spare Parts</Navigation.LogoText>
             </Navigation.Logo>
           </Navigation.Panel>
+          {authUser && authUser.role && (
+            <Navigation.Panel>{`Hi, ${authUser.nick}`}</Navigation.Panel>
+          )}
           <Navigation.Panel>{children}</Navigation.Panel>
         </Navigation>
       </Navigation.Container>
