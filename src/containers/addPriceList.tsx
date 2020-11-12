f2a74e67092fb47fc61846bd2a0270558745fb6d
krystek77 <krystian.wrona77@gmail.com>
2020-11-12 16:21:08 +0100

    Listen for real-time updates to price lists

--- src/containers/addPriceList.tsx
+++ src/containers/addPriceList.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
+import { dataBase } from '../lib/firebase';
 import { Form, CustomButton } from '../components';
+import { getAuthUser } from '../helpers';
 
-export const AddPriceList = ({ showAddPriceList = false }) => {
+interface IAddPriceList {
+  showAddPriceList?: boolean;
+}
+interface IPriceList {
+  name: string;
+  userID: string;
+  added: string;
+}
+export const AddPriceList: React.FC<IAddPriceList> = ({
+  showAddPriceList = false,
+}) => {
   const [priceListName, setPriceListName] = React.useState<string>('');
-  const [show, setShow] = React.useState(showAddPriceList);
+  const [show, setShow] = React.useState<boolean>(showAddPriceList);
+
+  const authUser = getAuthUser();
+
+  const handleAddPriceList = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const newPriceList: IPriceList = {
+      name: priceListName,
+      userID: authUser.userID,
+      added: new Date().toISOString().slice(0, 10),
+    };
+
+    dataBase
+      .collection('pricelists')
+      .add(newPriceList)
+      .then((result) => {
+        console.log(result.id);
+        console.log('PRICE LIST ADDED SUCCESSFULLY');
+      })
+      .catch((error) => console.log(error.message));
+  };
+
   return (
     <React.Fragment>
       {show && (
         <Form size={'addPriceList'}>
           <Form.Title>Add Price List</Form.Title>
-          <Form.BaseForm
-            onSubmit={(e) => {
-              e.preventDefault();
-              console.log('ADD PRICE LIST');
-            }}
-          >
+          <Form.BaseForm onSubmit={(e) => handleAddPriceList(e)}>
             <Form.Input
               type='text'
               id='priceListName'
@@ -33,7 +61,10 @@ export const AddPriceList = ({ showAddPriceList = false }) => {
               <Form.CustomButton
                 type='button'
                 btn={'CANCEL'}
-                onClick={() => console.log('CLOSE FORM TO ADD PRICELIST')}
+                onClick={() => {
+                  setPriceListName('');
+                  setShow(false);
+                }}
               >
                 CANCEL
               </Form.CustomButton>
@@ -42,12 +73,7 @@ export const AddPriceList = ({ showAddPriceList = false }) => {
         </Form>
       )}
       <Form.InputsGroup>
-        <CustomButton
-          onClick={() => {
-            console.log('OPEN FORM TO ADD PRICELIST');
-            setShow(!show);
-          }}
-        >
+        <CustomButton onClick={() => setShow(!show)}>
           +ADD PRICE LIST
         </CustomButton>
       </Form.InputsGroup>
--- src/hooks/usePriceLists.ts
+++ src/hooks/usePriceLists.ts
@@ -1,38 +1,105 @@
 import React from 'react';
 import { dataBase } from '../lib/firebase';
 //selected: '' than all priceLists, userID then for only given user
-interface IPriceLists {
+interface IPriceList {
   priceListID: string;
   name: string;
   userID: string;
+  added: string | undefined;
 }
 export const usePriceLists = (selected: string) => {
-  const [priceLists, setPriceLists] = React.useState<IPriceLists[]>([]);
+  const [priceLists, setPriceLists] = React.useState<IPriceList[]>([]);
 
   React.useEffect(() => {
-    let query = dataBase.collection('pricelists').get();
-    query =
+    console.log('[usePriceList] - useEffect');
+
+    let unsubscribe =
       selected !== ''
         ? dataBase
             .collection('pricelists')
             .where('userID', '==', selected)
-            .get()
-        : query;
-    query
-      .then((snapshot) => {
-        const priceLists: IPriceLists[] = [];
-        snapshot.forEach((doc) => {
-          const priceList: IPriceLists = {
-            priceListID: doc.id,
-            name: doc.data().name,
-            userID: doc.data().userID,
-          };
-          priceLists.push(priceList);
-        });
-        setPriceLists(priceLists);
-      })
-      .catch(() => console.log('CAN NOT GET PRICELISTS CATEGORIES'));
-    return () => {};
+            .onSnapshot(
+              (snapshot) => {
+                let priceLists = snapshot.docs.map((doc) => {
+                  return {
+                    priceListID: doc.id,
+                    name: doc.data().name,
+                    userID: doc.data().userID,
+                    added: doc.data().added,
+                  };
+                });
+                setPriceLists(priceLists);
+              },
+              (error) => console.log(error.message)
+            )
+        : dataBase.collection('pricelists').onSnapshot(
+            (snapshot) => {
+              let priceLists = snapshot.docs.map((doc) => {
+                return {
+                  priceListID: doc.id,
+                  name: doc.data().name,
+                  userID: doc.data().userID,
+                  added: doc.data().added,
+                };
+              });
+              setPriceLists(priceLists);
+            },
+            (error) => console.log(error.message)
+          );
+
+    // if (selected !== '') {
+    //   let query = dataBase
+    //     .collection('pricelists')
+    //     .where('userID', '==', selected)
+    //     .onSnapshot((snapshot) => {
+    //       console.log(snapshot);
+    //       let priceLists = snapshot.docs.map((doc) => {
+    //         return {
+    //           priceListID: doc.id,
+    //           name: doc.data().name,
+    //           userID: doc.data().userID,
+    //         };
+    //       });
+    //       setPriceLists(priceLists);
+    //     });
+    // } else {
+    //   let query = dataBase.collection('pricelists').onSnapshot((snapshot) => {
+    //     console.log(snapshot);
+    //     let priceLists = snapshot.docs.map((doc) => {
+    //       return {
+    //         priceListID: doc.id,
+    //         name: doc.data().name,
+    //         userID: doc.data().userID,
+    //       };
+    //     });
+    //     setPriceLists(priceLists);
+    //   });
+    // }
+    // let query = dataBase.collection('pricelists').get();
+    // query =
+    //   selected !== ''
+    //     ? dataBase
+    //         .collection('pricelists')
+    //         .where('userID', '==', selected)
+    //         .get()
+    //     : query;
+    // query
+    //   .then((snapshot) => {
+    //     const priceLists: IPriceLists[] = [];
+    //     snapshot.forEach((doc) => {
+    //       const priceList: IPriceLists = {
+    //         priceListID: doc.id,
+    //         name: doc.data().name,
+    //         userID: doc.data().userID,
+    //       };
+    //       priceLists.push(priceList);
+    //     });
+    //     setPriceLists(priceLists);
+    //   })
+    //   .catch(() => console.log('CAN NOT GET PRICELISTS CATEGORIES'));
+    return () => {
+      unsubscribe();
+    };
   }, [selected]);
   return { priceLists, setPriceLists };
 };
