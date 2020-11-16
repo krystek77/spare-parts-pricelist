08cf5179a5daff67a19f15a0008a5f5f50b8e6ba
krystek77 <krystian.wrona77@gmail.com>
2020-11-16 20:59:11 +0100

    Wrap searchForm through react.memo

--- src/containers/searchForm.tsx
+++ src/containers/searchForm.tsx
@@ -6,25 +6,27 @@ interface ISearchFormContainer {
   setSearch: (search: string) => void;
 }
 
-export const SearchFormContainer: React.FC<ISearchFormContainer> = (props) => {
-  const { search, setSearch } = props;
-  return (
-    <Form size='search'>
-      <Form.Title>Search By Name</Form.Title>
-      <Form.BaseForm>
-        <Form.InputsGroup>
-          <Form.InputLabel htmlFor='search'>Search name:</Form.InputLabel>
-          <Form.Input
-            type='text'
-            name='search'
-            id='search'
-            value={search}
-            placeholder='Enter search text'
-            onChange={(e) => setSearch(e.currentTarget.value)}
-            onKeyDown={(e) => setSearch(e.currentTarget.value)}
-          />
-        </Form.InputsGroup>
-      </Form.BaseForm>
-    </Form>
-  );
-};
+export const SearchFormContainer: React.FC<ISearchFormContainer> = React.memo(
+  (props) => {
+    const { search, setSearch } = props;
+    return (
+      <Form size='search'>
+        <Form.Title>Search By Name</Form.Title>
+        <Form.BaseForm>
+          <Form.InputsGroup>
+            <Form.InputLabel htmlFor='search'>Search name:</Form.InputLabel>
+            <Form.Input
+              type='text'
+              name='search'
+              id='search'
+              value={search}
+              placeholder='Enter search text'
+              onChange={(e) => setSearch(e.currentTarget.value)}
+              onKeyDown={(e) => setSearch(e.currentTarget.value)}
+            />
+          </Form.InputsGroup>
+        </Form.BaseForm>
+      </Form>
+    );
+  }
+);
