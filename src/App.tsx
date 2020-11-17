8a81228929f4e269c898c85a869bf498ff90f9ef
krystek77 <krystian.wrona77@gmail.com>
2020-11-17 19:04:08 +0100

    Add dynamically importing all pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,164 +1,141 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/home';
-import BrowsePage from './pages/browse';
-import AdminPage from './pages/admin';
-import SigninPage from './pages/signin';
-import EditUserPage from './pages/editUser';
-import EditAdminPage from './pages/editAdmin';
-import UserPage from './pages/user';
-import AddUser from './pages/addUser';
-import BrowseUSersPage from './pages/browseUsers';
-import AddSparePart from './pages/addSparePart';
-import EditSparePartPage from './pages/editSparePart';
-import AdminProfilePage from './pages/adminProfile';
-import ResetPasswordPage from './pages/resetPassword';
-import NoMatchPage from './pages/nomatch';
-// import {
-//   HomePage,
-//   BrowsePage,
-//   AdminPage,
-//   SigninPage,
-//   UserPage,
-//   EditUserPage,
-//   AddUser,
-//   BrowseUSersPage,
-//   AddSparePart,
-//   EditSparePartPage,
-//   EditAdminPage,
-//   AdminProfilePage,
-//   ResetPasswordPage,
-//   NoMatchPage,
-// } from './pages';
 import * as ROUTES from './constants/routes';
 import { useAuth } from './hooks';
 import { RedirectUser, ProtectedRoute, ROLES } from './helpers';
+import { Spinner } from './components';
+
+const HomePage = React.lazy(() => import('./pages/home'));
+const BrowsePage = React.lazy(() => import('./pages/browse'));
+const AdminPage = React.lazy(() => import('./pages/admin'));
+const SigninPage = React.lazy(() => import('./pages/signin'));
+const EditUserPage = React.lazy(() => import('./pages/editUser'));
+const EditAdminPage = React.lazy(() => import('./pages/editAdmin'));
+const UserPage = React.lazy(() => import('./pages/user'));
+const AddUser = React.lazy(() => import('./pages/addUser'));
+const BrowseUSersPage = React.lazy(() => import('./pages/browseUsers'));
+const AddSparePart = React.lazy(() => import('./pages/addSparePart'));
+const EditSparePartPage = React.lazy(() => import('./pages/editSparePart'));
+const AdminProfilePage = React.lazy(() => import('./pages/adminProfile'));
+const ResetPasswordPage = React.lazy(() => import('./pages/resetPassword'));
+const NoMatchPage = React.lazy(() => import('./pages/nomatch'));
 
 export function App() {
   const { authUser } = useAuth();
   return (
     <Router>
-      {/** <div className="root"> all components</div> */}
-      <Switch>
-        <RedirectUser
-          authUser={authUser}
-          loggedPath={ROUTES.BROWSE}
-          path={ROUTES.SIGNIN}
-        >
-          <SigninPage />
-        </RedirectUser>
-        {/* <Route path={ROUTES.SIGNIN}>
-          <SigninPage />
-        </Route> */}
-        <ProtectedRoute
-          role={ROLES.USER}
-          authUser={authUser}
-          path={ROUTES.USER_EDIT}
-        >
-          <EditUserPage />
-        </ProtectedRoute>
-        <ProtectedRoute
-          role={ROLES.USER}
-          authUser={authUser}
-          path={ROUTES.USER}
-        >
-          <UserPage />
-        </ProtectedRoute>
+      <React.Suspense fallback={<Spinner>Loading page...</Spinner>}>
+        <Switch>
+          <RedirectUser
+            authUser={authUser}
+            loggedPath={ROUTES.BROWSE}
+            path={ROUTES.SIGNIN}
+          >
+            <SigninPage />
+          </RedirectUser>
 
-        {/* <Route path={ROUTES.USER}>
-          <UserPage />
-        </Route> */}
-        <ProtectedRoute
-          path={`${ROUTES.EDIT_SPARE_PART}/:sparePartID`}
-          authUser={authUser}
-          role={ROLES.ADMIN}
-        >
-          <EditSparePartPage />
-        </ProtectedRoute>
-        <ProtectedRoute
-          path={ROUTES.ADMIN_PROFILE}
-          authUser={authUser}
-          role={ROLES.ADMIN}
-        >
-          <AdminProfilePage />
-        </ProtectedRoute>
-        <ProtectedRoute
-          path={ROUTES.EDIT_ADMIN}
-          authUser={authUser}
-          role={ROLES.ADMIN}
-        >
-          <EditAdminPage />
-        </ProtectedRoute>
-        <ProtectedRoute
-          exact
-          path={ROUTES.ADD_SPARE_PART}
-          authUser={authUser}
-          role={ROLES.ADMIN}
-        >
-          <AddSparePart />
-        </ProtectedRoute>
+          <ProtectedRoute
+            role={ROLES.USER}
+            authUser={authUser}
+            path={ROUTES.USER_EDIT}
+          >
+            <EditUserPage />
+          </ProtectedRoute>
 
-        <ProtectedRoute
-          path={ROUTES.ADD_USER}
-          authUser={authUser}
-          role={ROLES.ADMIN}
-        >
-          <AddUser />
-        </ProtectedRoute>
+          <ProtectedRoute
+            role={ROLES.USER}
+            authUser={authUser}
+            path={ROUTES.USER}
+          >
+            <UserPage />
+          </ProtectedRoute>
 
-        {/* <Route path={ROUTES.ADD_USER}>
-          <AddUser />
-        </Route> */}
-        <ProtectedRoute
-          path={ROUTES.BROWSE_USERS}
-          authUser={authUser}
-          role={ROLES.ADMIN}
-        >
-          <BrowseUSersPage />
-        </ProtectedRoute>
+          <ProtectedRoute
+            path={`${ROUTES.EDIT_SPARE_PART}/:sparePartID`}
+            authUser={authUser}
+            role={ROLES.ADMIN}
+          >
+            <EditSparePartPage />
+          </ProtectedRoute>
 
-        <ProtectedRoute
-          exact
-          role={ROLES.ADMIN}
-          authUser={authUser}
-          path={ROUTES.ADMIN}
-        >
-          <AdminPage />
-        </ProtectedRoute>
-        {/* <Route path={ROUTES.ADMIN}>
-          <AdminPage />
-        </Route> */}
-        <ProtectedRoute
-          authUser={authUser}
-          role={authUser.role === ROLES.ADMIN ? ROLES.ADMIN : ROLES.USER}
-          path={ROUTES.BROWSE}
-        >
-          <BrowsePage />
-        </ProtectedRoute>
-        <ProtectedRoute
-          authUser={authUser}
-          role={authUser.role === ROLES.ADMIN ? ROLES.ADMIN : ROLES.USER}
-          path={ROUTES.RESET_PASSWORD}
-        >
-          <ResetPasswordPage />
-        </ProtectedRoute>
-        {/* <Route path={ROUTES.BROWSE}>
-          <BrowsePage />
-        </Route> */}
-        <RedirectUser
-          authUser={authUser}
-          loggedPath={ROUTES.BROWSE}
-          path={ROUTES.HOME}
-        >
-          <HomePage />
-        </RedirectUser>
-        {/* <Route path={ROUTES.HOME}>
-          <HomePage />
-        </Route> */}
-        <Route>
-          <NoMatchPage />
-        </Route>
-      </Switch>
+          <ProtectedRoute
+            path={ROUTES.ADMIN_PROFILE}
+            authUser={authUser}
+            role={ROLES.ADMIN}
+          >
+            <AdminProfilePage />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            path={ROUTES.EDIT_ADMIN}
+            authUser={authUser}
+            role={ROLES.ADMIN}
+          >
+            <EditAdminPage />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            exact
+            path={ROUTES.ADD_SPARE_PART}
+            authUser={authUser}
+            role={ROLES.ADMIN}
+          >
+            <AddSparePart />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            path={ROUTES.ADD_USER}
+            authUser={authUser}
+            role={ROLES.ADMIN}
+          >
+            <AddUser />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            path={ROUTES.BROWSE_USERS}
+            authUser={authUser}
+            role={ROLES.ADMIN}
+          >
+            <BrowseUSersPage />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            exact
+            role={ROLES.ADMIN}
+            authUser={authUser}
+            path={ROUTES.ADMIN}
+          >
+            <AdminPage />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            authUser={authUser}
+            role={authUser.role === ROLES.ADMIN ? ROLES.ADMIN : ROLES.USER}
+            path={ROUTES.BROWSE}
+          >
+            <BrowsePage />
+          </ProtectedRoute>
+          <ProtectedRoute
+            authUser={authUser}
+            role={authUser.role === ROLES.ADMIN ? ROLES.ADMIN : ROLES.USER}
+            path={ROUTES.RESET_PASSWORD}
+          >
+            <ResetPasswordPage />
+          </ProtectedRoute>
+
+          <RedirectUser
+            authUser={authUser}
+            loggedPath={ROUTES.BROWSE}
+            path={ROUTES.HOME}
+          >
+            <HomePage />
+          </RedirectUser>
+
+          <Route>
+            <NoMatchPage />
+          </Route>
+        </Switch>
+      </React.Suspense>
     </Router>
   );
 }
