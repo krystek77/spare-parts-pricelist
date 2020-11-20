1f71a18ab8a87baf1850da4d2275900ae744ae32
krystek77 <krystian.wrona77@gmail.com>
2020-11-20 08:52:26 +0100

    Delete admin account and all him data

--- src/pages/adminProfile.tsx
+++ src/pages/adminProfile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { dataBase, auth } from '../lib/firebase';
 import {
   MainContainer,
   NavigationContainer,
@@ -18,14 +19,75 @@ export const AdminProfilePage: React.FC<IAdminProfilePage> = () => {
   /**
    * TODO:
    * 1. Delete admin
-   * - find all pricelists belong to current admin
-   * - delete all spareparts  belong to these priceLists
+   * - delete all spareparts  belong to current admin
    * - delete all pricelists belong to current admin
    * - delete admin details and than
    * - delete admin account
    */
+
   const handleDeleteAdmin = () => {
-    console.log('DELETE ADMIN');
+    // console.log('DELETE ADMIN');
+    const currentAdminID = authUser.userID;
+    dataBase
+      .collection('spare-parts')
+      .where('userID', '==', currentAdminID)
+      .get()
+      .then((collectionSpareParts) => {
+        // console.log(collectionSpareParts.docs);
+        if (collectionSpareParts.size === 0) {
+          // console.log(`${authUser.nick} have not got any spareparts`);
+          return;
+        } else {
+          const batch = dataBase.batch();
+          collectionSpareParts.docs.forEach((sparepart) => {
+            batch.delete(sparepart.ref);
+          });
+          // console.log(
+          //   `${authUser.nick} have got spareparts, and they have been deleted`
+          // );
+          return batch.commit();
+        }
+      })
+      .then(() => {
+        // console.log('Now I can delete pricelist');
+        return dataBase
+          .collection('pricelists')
+          .where('userID', '==', currentAdminID)
+          .get();
+      })
+      .then((collectionPricelists) => {
+        // console.log(collectionPricelists.docs);
+        if (collectionPricelists.size === 0) {
+          // console.log(`${authUser.nick} have not got any pricelists`);
+          return;
+        } else {
+          const batch = dataBase.batch();
+          collectionPricelists.docs.forEach((pricelist) => {
+            batch.delete(pricelist.ref);
+          });
+          // console.log(
+          //   `${authUser.nick} have got pricelists, and they have been deleted`
+          // );
+          return batch.commit();
+        }
+      })
+      .then(() => {
+        // console.log('Now I can delete user details');
+        return dataBase.collection('users').doc(currentAdminID).delete();
+      })
+      .then(() => {
+        // console.log('Now I can delete user account');
+        const user = auth.currentUser;
+        if (user) {
+          return user.delete();
+        }
+      })
+      .then(() => {
+        // console.log(
+        //   'User account and all data belong to him and created by him have been deleted successfully'
+        // );
+      })
+      .catch((error) => console.log(error.message));
   };
   return (
     <React.Fragment>
